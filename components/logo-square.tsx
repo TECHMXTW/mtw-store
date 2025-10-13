d0d1ae66d2d1d90bafaa3e165a2f89486f12b7e4
Vmanuee <vmanuee@gmail.com>
2025-10-12 23:33:36 -0600

    Update logo-square.tsx

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,22 +1,23 @@
+import Image from 'next/image';
 import clsx from 'clsx';
-import LogoIcon from './icons/logo';
 
 export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
   return (
     <div
       className={clsx(
-        'flex flex-none items-center justify-center',
+        'flex flex-none items-center justify-center overflow-hidden',
         {
           'h-[40px] w-[40px] rounded-xl': !size,
           'h-[30px] w-[30px] rounded-lg': size === 'sm'
         }
       )}
     >
-      <LogoIcon
-        className={clsx({
-          'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
-        })}
+      <Image
+        src="/logo-mxtw.png" // asegúrate de subir este archivo a /public
+        alt="MXTW"
+        width={size === 'sm' ? 30 : 40}
+        height={size === 'sm' ? 30 : 40}
+        priority
       />
     </div>
   );
